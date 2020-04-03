0d6183ed9e4422f42ac9a81a7f51b27db433ba66
Atrylon <berenger.desgardin@gmail.com>
2020-04-03 14:46:28 +0200

    début menu

--- Classes/Player.py
+++ Classes/Player.py
@@ -1,5 +1,22 @@
 class Card:
-    def __init__(self, index, hp, shield):
+    def __init__(self, index):
+        # Joueur 1 ou Joueur 2
         self.index = index
-        self.hp = hp
-        self.shield = shield
+        self.hp = 100
+        self.shield = 30
+        # Tableau qui contient la main du joueur - 7 cartes max
+        self.hand = []
+        self.gold_generation = 1
+        self.gold_stock = 0
+        self.mana_generation = 1
+        self.mana_stock = 0
+        self.power_generation = 1
+        self.power_stock = 0
+
+    def changeHP(self, hp_change):
+        self.hp = self.hp - hp_change
+        print("Vous avez actuellement " + self.hp + " pvs !")
+
+    def changeShield(self, shield_change):
+        self.shield = self.shield - shield_change
+        print("Vous avez actuellement " + self.shield + " points de bouclier !")
--- Game.py
+++ Game.py
@@ -1,21 +1,102 @@
+import sys
 import pygame
 from pygame.locals import *
 
 pygame.init()
+pygame.font.init()
 
-#Ouverture de la fenêtre Pygame
-fenetre = pygame.display.set_mode((1280, 910), )
 
-#Chargement et collage du fond
-fond = pygame.image.load("images/background_gwent.jpg").convert()
-fenetre.blit(fond, (0,0))
+pygame.display.set_caption('IPSSI Card Game')
+screen = pygame.display.set_mode((900, 500))
+font = pygame.font.SysFont('Helvetic', 50)
+click = False
 
-#Rafraîchissement de l'écran
-pygame.display.flip()
 
-# Variable qui continue la boucle si = 1, stoppe si = 0
-continuer = 1
-while continuer:
-    for event in pygame.event.get():  # On parcours la liste de tous les événements reçus
-        if event.type == QUIT:  # Si un de ces événements est de type QUIT
-            continuer = 0  # On arrête la boucle
+def draw_text(text, font, color, surface, x, y):
+    textobj = font.render(text, 1, color)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    surface.blit(textobj, textrect)
+
+
+def main_menu():
+
+    while True:
+        screen.fill((192, 192, 192))
+        draw_text('IPSSI Card Game', font, (0, 0, 0), screen, 50, 20)
+        mx, my = pygame.mouse.get_pos()
+        button_1 = pygame.Rect(50, 100, 200, 50)
+        button_2 = pygame.Rect(50, 200, 200, 50)
+        button_3 = pygame.Rect(50, 300, 200, 50)
+
+        if button_1.collidepoint((mx, my)):
+            if click:
+                game()
+        if button_2.collidepoint((mx, my)):
+            if click:
+                options()
+        if button_3.collidepoint((mx, my)):
+            if click:
+                exit()
+
+        pygame.draw.rect(screen, (255, 0, 0), button_1)
+        pygame.draw.rect(screen, (255, 0, 0), button_2)
+        pygame.draw.rect(screen, (255, 0, 0), button_3)
+
+        screen.blit(font.render('Jouer', True, (0, 0, 0)), (88, 110))
+        screen.blit(font.render('Options', True, (0, 0, 0)), (88, 210))
+        screen.blit(font.render('Quitter', True, (0, 0, 0)), (88, 310))
+        pygame.display.update()
+
+        click = False
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+            if event.type == MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click = True
+
+        pygame.display.update()
+
+
+def game():
+    running = True
+    while running:
+        screen = pygame.display.set_mode((1280, 910))
+        fond = pygame.image.load("ressources/images/background_gwent.jpg").convert()
+        screen.blit(fond, (0, 0))
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+
+        pygame.display.update()
+
+
+def options():
+    running = True
+    while running:
+        screen.fill((192, 192, 192))
+
+        draw_text('options', font, (255, 255, 255), screen, 20, 20)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+
+        pygame.display.update()
+
+
+main_menu()
