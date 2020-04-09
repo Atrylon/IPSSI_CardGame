e98272933e332dc5fd10e00c31f9b2dc6d2ab986
Atrylon <berenger.desgardin@gmail.com>
2020-04-09 17:38:33 +0200

    create requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='CardGame_Python_Ipssi',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/Atrylon/IPSSI_CardGame',
+    license='',
+    author='Berenger & Nathan',
+    author_email='berenger.desgardin@gmail.com',
+    description='Projet à l\'ipssi d\'un jeu de carte en python avec Pygame'
+)
